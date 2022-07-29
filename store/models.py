d249ca096d6325abcc7b02fe6c1c3066dbf56773
Istamjon7 <majidovistam165@gmail.com>
2022-07-29 19:12:59 +0500

    'models'

--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='Category',null=True,blank=True)
+
+    @property
+    def imageURL(self):
+        try:
+            return self.image.url
+        except:
+            pass
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Product')
+    price = models.FloatField()
+    description = models.TextField()
+    color = models.ManyToManyField(Color,null=True,blank=True)
--- stroykaa/settings.py
+++ stroykaa/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -127,7 +129,7 @@ STATICFILES_DIRS = (
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
