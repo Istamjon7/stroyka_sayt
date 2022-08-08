a928372c876d81240adb8974e6701747a5b5277e
Istamjon7 <majidovistam165@gmail.com>
2022-08-08 16:32:30 +0500

    'views'

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index, name='index')
+    path('',index, name='index'),
+    path('product/',product, name='product'),
+    path('cart/',cart, name='cart'),
+    path('about/',about, name='about'),
+    path('blog-grid/',blog_grid, name='blog-grid'),
+    path('wishlist/',wishlist, name='wishlist'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def product(request):
+    return render(request, 'product.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+def about(request):
+    return render(request, 'about-us.html')
+
+def blog_grid(request):
+    return render(request, 'blog-grid.html')
